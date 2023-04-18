9f1e13e6d78b78ad676c6b190ab72979b79cc6ef
reuben21 <reuben211999@gmail.com>
2023-04-18 01:33:33 -0400

    Updated Realtional Model and Merge Conflict

--- 02RelationModel.sql
+++ 02RelationModel.sql
@@ -23,15 +23,17 @@ GO
 --DROP DATABASE NYC_311_REQUESTS;
 --GO
 
-CREATE DATABASE NYC_311_REQUESTS;
+CREATE DATABASE NYC_311;
 GO
 
 
-USE NYC_311_REQUESTS;
+USE NYC_311;
 GO
 
 -- RELATIONAL MODEL
+--==============AGENCY TABLE==============--
 DROP TABLE Agency;
+GO
 
 
 CREATE TABLE Agency
@@ -60,7 +62,7 @@ GO
 SELECT *
 FROM Agency;
 GO
-
+--==============COMPLAINT TABLE==============--
 DROP TABLE Complaint;
 GO
 
@@ -92,10 +94,11 @@ from Complaint;
 GO
 
 --==============CITY TABLE==============--
+DROP TABLE City;
 CREATE TABLE City
 (
-    ID                  INT PRIMARY KEY IDENTITY,
-    City                VARCHAR(255),
+    ID   INT PRIMARY KEY IDENTITY,
+    CityName VARCHAR(255),
 );
 GO
 
@@ -104,12 +107,8 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    INSERT INTO City (
-                                        City
-                                       
-                                       )
-    SELECT DISTINCT
-           [City]      
+    INSERT INTO City (CityName)
+    SELECT DISTINCT [City]
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023];
 END
 GO
@@ -117,15 +116,16 @@ GO
 EXEC City_Extract;
 GO
 
-SELECT * FROM CITY;
+SELECT *
+FROM CITY;
 GO
 
 --==============COORDINATES TABLE==============--
 CREATE TABLE Coordinates
 (
-    ID                  INT PRIMARY KEY IDENTITY,
-    Latitude            FLOAT,
-    Longitude           FLOAT
+    ID        INT PRIMARY KEY IDENTITY,
+    Latitude  FLOAT,
+    Longitude FLOAT
 );
 GO
 
@@ -135,8 +135,8 @@ BEGIN
     SET NOCOUNT ON;
 
     INSERT INTO Coordinates
-    SELECT DISTINCT
-           [Latitude],[Longitude]      
+    SELECT DISTINCT [Latitude],
+                    [Longitude]
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023];
 END
 GO
@@ -145,7 +145,8 @@ EXEC Coordinates_Extract;
 GO
 
 
-SELECT * FROM Coordinates;
+SELECT *
+FROM Coordinates;
 GO
 
 --==============STREET ADDRESS TABLE==============--
@@ -165,18 +166,18 @@ BEGIN
     SET NOCOUNT ON;
 
     INSERT INTO StreetAddress (StreetName, CrossStreet1, CrossStreet2)
-    SELECT DISTINCT 
-              [Street_Name]
-      ,[Cross_Street_1]
-      ,[Cross_Street_2]
+    SELECT DISTINCT [Street_Name]
+                  , [Cross_Street_1]
+                  , [Cross_Street_2]
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
-    
+
 END
 GO
 
 EXEC StreetAddress_Extract;
 
-SELECT * FROM StreetAddress;
+SELECT *
+FROM StreetAddress;
 
 
 --==============SERVICE REQUEST LOCATION TABLE==============--
@@ -192,9 +193,9 @@ CREATE TABLE ServiceRequestLocation
     IntersectionStreet2 VARCHAR(255),
     AddressType         VARCHAR(255),
     Landmark            VARCHAR(255),
-	StreetAddressID      INT REFERENCES StreetAddress(ID),
-	CityID INT FOREIGN KEY REFERENCES City(ID),
-	CoordinatesID INT FOREIGN KEY REFERENCES Coordinates(ID),
+    StreetAddressID     INT REFERENCES StreetAddress (ID),
+    CityID              INT FOREIGN KEY REFERENCES City (ID) ,
+    CoordinatesID       INT FOREIGN KEY REFERENCES Coordinates (ID),
 );
 GO
 
@@ -209,27 +210,26 @@ BEGIN
                                         IntersectionStreet2,
                                         AddressType,
                                         Landmark,
-										StreetAddressID,
-										CityID,
-										CoordinatesID
-                                       )
+                                        StreetAddressID,
+                                        CityID,
+                                        CoordinatesID)
     SELECT [Incident_Zip],
            [Incident_Address],
            [Intersection_Street_1],
            [Intersection_Street_2],
            [Address_Type],
            [Landmark],
-		   sa.ID,
-		   City.ID,
-		   Coordinates.ID
-           
+           sa.ID,
+           City.ID,
+           Coordinates.ID
+
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023] nyc
-	INNER JOIN StreetAddress sa ON sa.StreetName = nyc.Street_Name
-	AND sa.CrossStreet1 = nyc.Cross_Street_1
-	AND sa.CrossStreet2 = nyc.Cross_Street_2
-	INNER JOIN City ON City.City = nyc.[City]
-	INNER JOIN Coordinates ON Coordinates.Latitude = nyc.[Latitude]
-	AND Coordinates.Longitude = nyc.[Longitude]
+             INNER JOIN StreetAddress sa ON sa.StreetName = nyc.Street_Name
+			AND sa.CrossStreet1 = nyc.Cross_Street_1
+			AND sa.CrossStreet2 = nyc.Cross_Street_2
+             INNER JOIN City ON City.CityName = nyc.[City]
+             INNER JOIN Coordinates ON Coordinates.Latitude = nyc.[Latitude]
+        AND Coordinates.Longitude = nyc.[Longitude]
 END
 GO
 
@@ -239,37 +239,15 @@ GO
 
 SELECT *
 FROM dbo.ServiceRequestLocation;
-
-SELECT DISTINCT IncidentAddress
-FROM dbo.ServiceRequestLocation;
-
-SELECT COUNT(*)
-FROM dbo.ServiceRequestLocation;
-
--- IMPORTANT
--- SELECT COUNT(*)
--- FROM (
---     SELECT srl.*
--- FROM (
---     SELECT DISTINCT IncidentZip, IncidentAddress
---     FROM dbo.ServiceRequestLocation
--- ) AS d
--- INNER JOIN dbo.ServiceRequestLocation AS srl
---     ON srl.IncidentZip = d.IncidentZip
---     AND srl.IncidentAddress = d.IncidentAddress
--- ) AS SR;
-
-
-SELECT *
-FROM dbo.ServiceRequestLocation;
 GO
+
 
 ---====================Geography Boundary=================----
 CREATE TABLE GeographicBoundary
 (
-    ID              INT PRIMARY KEY IDENTITY,
-    GeographicType  VARCHAR(50),
-    Number          BIGINT
+    ID             INT PRIMARY KEY IDENTITY,
+    GeographicType VARCHAR(50),
+    Number         BIGINT
 );
 GO
 
@@ -277,21 +255,61 @@ CREATE OR ALTER PROCEDURE GeographicData_Extraction
 AS
 BEGIN
     INSERT INTO GeographicBoundary (GeographicType, Number)
-    SELECT DISTINCT 'Community District',  Community_Districts FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
+    SELECT DISTINCT 'Community District', Community_Districts
+    FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
     UNION ALL
-    SELECT DISTINCT 'Borough Boundary', Borough_Boundaries FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
+    SELECT DISTINCT 'Borough Boundary', Borough_Boundaries
+    FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
     UNION ALL
-    SELECT DISTINCT 'City Council District', City_Council_Districts FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
+    SELECT DISTINCT 'City Council District', City_Council_Districts
+    FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
     UNION ALL
-    SELECT DISTINCT 'Police Precinct', Police_Precincts FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
+    SELECT DISTINCT 'Police Precinct', Police_Precincts
+    FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023]
 END
 GO
 
 EXEC GeographicData_Extraction;
 GO
 
-SELECT * FROM GeographicBoundary;
+SELECT *
+FROM GeographicBoundary;
 GO
+---====================Resolution Table =================----
+DROP TABLE Resolution;
+
+CREATE TABLE Resolution
+(
+    ID                   INT PRIMARY KEY IDENTITY,
+	ServiceKeyID                INT UNIQUE,
+    ResolutionDescription        VARCHAR(MAX),
+    ResolutionActionUpdatedDate DATETIME,
+    ResolutionStatus        VARCHAR(255),
+);
+GO
+
+
+CREATE OR ALTER PROCEDURE Resolution_Extract
+AS
+BEGIN
+    INSERT INTO Resolution (	ServiceKeyID,
+                                ResolutionDescription,
+                                ResolutionActionUpdatedDate,
+                                ResolutionStatus
+								)
+    SELECT Unique_Key,
+           [Resolution_Description],
+            Resolution_Action_Updated_Date,
+           [Status]
+    FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023] r
+            
+         
+END
+GO
+
+EXEC Resolution_Extract;
+
+SELECT * FROM Resolution;
 
 ---====================Service Request=================----
 DROP TABLE ServiceRequest;
@@ -299,61 +317,82 @@ DROP TABLE ServiceRequest;
 CREATE TABLE ServiceRequest
 (
     UniqueKey                   INT PRIMARY KEY IDENTITY,
-    ServiceKeyID                INT Unique,
+    ServiceKeyID                INT UNIQUE,
     CreatedDate                 DATETIME,
     ClosedDate                  DATETIME,
     DueDate                     DATETIME,
-    ResolutionDescription       VARCHAR(MAX),
-    ResolutionActionUpdatedDate DATETIME,
-    ServiceRequestStatus        VARCHAR(255),
+    ResolutionID				INT FOREIGN KEY (ResolutionID) REFERENCES Resolution (ID),
     AgencyID                    INT FOREIGN KEY (AgencyID) REFERENCES Agency (ID),
     LocationID                  INT FOREIGN KEY (LocationID) REFERENCES ServiceRequestLocation (ID),
-	CommunityDistrictID INT FOREIGN KEY REFERENCES GeographicBoundary(ID),
-	BoroughBoundaryID INT FOREIGN KEY REFERENCES GeographicBoundary(ID),
-	CityCouncilDistrictID INT FOREIGN KEY REFERENCES GeographicBoundary(ID),
-	PolicePrecinctID INT FOREIGN KEY REFERENCES GeographicBoundary(ID), 
-    
+    CommunityDistrictID         INT FOREIGN KEY REFERENCES GeographicBoundary (ID),
+    BoroughBoundaryID           INT FOREIGN KEY REFERENCES GeographicBoundary (ID),
+    CityCouncilDistrictID       INT FOREIGN KEY REFERENCES GeographicBoundary (ID),
+    PolicePrecinctID            INT FOREIGN KEY REFERENCES GeographicBoundary (ID),
 );
 GO
+
 
 CREATE OR ALTER PROCEDURE ServiceRequests_Extract
 AS
 BEGIN
+    SET NOCOUNT ON;
     INSERT INTO ServiceRequest (ServiceKeyID,
                                 CreatedDate,
                                 ClosedDate,
                                 DueDate,
-                                ResolutionDescription,
-                                ResolutionActionUpdatedDate,
-                                ServiceRequestStatus,
+                                ResolutionID,
                                 AgencyID,
                                 LocationID,
                                 CommunityDistrictID,
                                 BoroughBoundaryID,
                                 CityCouncilDistrictID,
                                 PolicePrecinctID)
-    SELECT Unique_Key,
-           Created_Date,
-           Closed_Date,
-           Due_Date,
-           Resolution_Description,
-           Resolution_Action_Updated_Date,
-           [Status],
+    SELECT r.Unique_Key,
+           r.Created_Date,
+           r.Closed_Date,
+           r.Due_Date,
+           rs.ID,
            a.ID AS AgencyID,
            l.ID AS LocationID,
-           g1.ID AS Community_District,
-           g2.ID AS Borough_Boundary,
-           g3.ID AS City_Council_District,
-           g4.ID AS Police_Precinct
+           g1.ID AS CommunityDistrictID,
+           g2.ID AS BoroughBoundaryID,
+           g3.ID AS CityCouncilDistrictID,
+           g4.ID AS PolicePrecinctID
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023] r
-             LEFT JOIN Agency a ON r.Agency = a.AgencyName
-             LEFT JOIN ServiceRequestLocation l
-                       ON r.Incident_Zip = l.IncidentZip
-                           AND r.Incident_Address = l.IncidentAddress
-             LEFT JOIN GeographicBoundary g1 ON r.Community_Districts = g1.Number AND g1.GeographicType = 'Community District'
-             LEFT JOIN GeographicBoundary g2 ON r.Borough_Boundaries = g2.Number AND g2.GeographicType = 'Borough Boundary'
-             LEFT JOIN GeographicBoundary g3 ON r.City_Council_Districts = g3.Number AND g3.GeographicType = 'City Council District'
-             LEFT JOIN GeographicBoundary g4 ON r.Police_Precincts = g4.Number AND g4.GeographicType = 'Police Precinct'
+    LEFT JOIN Resolution rs
+        ON r.Unique_Key = rs.ServiceKeyID
+    LEFT JOIN Agency a
+        ON r.Agency = a.AgencyName
+    LEFT JOIN (
+        SELECT IncidentZip, IncidentAddress, ID
+        FROM ServiceRequestLocation
+    ) l
+        ON r.Incident_Zip = l.IncidentZip
+        AND r.Incident_Address = l.IncidentAddress
+    LEFT JOIN (
+        SELECT Number, ID
+        FROM GeographicBoundary
+        WHERE GeographicType = 'Community District'
+    ) g1
+        ON r.Community_Districts = g1.Number
+    LEFT JOIN (
+        SELECT Number, ID
+        FROM GeographicBoundary
+        WHERE GeographicType = 'Borough Boundary'
+    ) g2
+        ON r.Borough_Boundaries = g2.Number
+    LEFT JOIN (
+        SELECT Number, ID
+        FROM GeographicBoundary
+        WHERE GeographicType = 'City Council District'
+    ) g3
+        ON r.City_Council_Districts = g3.Number
+    LEFT JOIN (
+        SELECT Number, ID
+        FROM GeographicBoundary
+        WHERE GeographicType = 'Police Precinct'
+    ) g4
+        ON r.Police_Precincts = g4.Number;
 END
 GO
 
@@ -372,10 +411,12 @@ CREATE TABLE ServiceRequest_Complaint
 (
     ServiceRequestID INT,
     ComplaintID      INT,
-    PRIMARY KEY (ServiceRequestID, ComplaintID),
-    FOREIGN KEY (ServiceRequestID) REFERENCES ServiceRequest (ServiceKeyID),
+    FOREIGN KEY (ServiceRequestID) REFERENCES ServiceRequest (UniqueKey),
     FOREIGN KEY (ComplaintID) REFERENCES Complaint (ID)
 );
+GO
+
+DROP TABLE ServiceRequest_Complaint;
 GO
 
 CREATE OR ALTER PROCEDURE ServiceRequest_Complaint_Insert
@@ -386,8 +427,8 @@ BEGIN
     INSERT INTO ServiceRequest_Complaint (ServiceRequestID, ComplaintID)
     SELECT s.UniqueKey, c.ID
     FROM [NYC_311_REQUESTS].[dbo].[311_JAN_2023_TO_MAR_2023] r
-        INNER JOIN ServiceRequest s ON r.Unique_Key = s.ServiceKeyID
-        INNER JOIN Complaint c ON r.Complaint_Type = c.ComplaintType
+             INNER JOIN ServiceRequest s ON r.Unique_Key = s.ServiceKeyID
+             INNER JOIN Complaint c ON r.Complaint_Type = c.ComplaintType
 END
 GO
 
